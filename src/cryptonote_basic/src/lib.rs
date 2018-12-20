3b18163a2fc618f013d6199e5fae242a0ec022a6
LINE <769483043@qq.com>
2018-12-20 14:16:42 +0800

    fix TxIn and TxOut lib.rs

--- src/cryptonote_basic/src/lib.rs
+++ src/cryptonote_basic/src/lib.rs
@@ -1,26 +1,27 @@
 extern crate xmr_crypto;
 
+pub use tx_in::TxIn;
+pub use tx_in_gen::TxInGen;
+pub use tx_in_to_key::TxInToKey;
+pub use tx_in_to_script::TxInToScript;
+pub use tx_in_to_script_hash::TxInToScriptHash;
+pub use tx_out_target::TxOutTarget;
+pub use tx_out_to_key::TxOutToKey;
+pub use tx_out_to_script::TxOutToScript;
+pub use tx_out_to_script_hash::TxOutToScriptHash;
 use xmr_crypto::crypto::Signature;
 use xmr_crypto::hash::Hash;
 
-//use crate::tx_in_gen::TxInGen;
-//use crate::tx_in_to_key::TxInToKey;
-//use crate::tx_in_to_script::TxInToScript;
-//use crate::tx_in_to_script_hash::TxInToScriptHash;
-//use crate::tx_out_to_key::TxOutToKey;
-//use crate::tx_out_to_script::TxOutToScript;
-//use crate::tx_out_to_script_hash::TxOutToScriptHash;
+mod tx_out_to_script;
+mod tx_out_to_script_hash;
+mod tx_out_to_key;
+mod tx_out_target;
 
-pub mod tx_out_to_script;
-pub mod tx_out_to_script_hash;
-pub mod tx_out_to_key;
-pub mod tx_out_target;
-
-pub mod tx_in_gen;
-pub mod tx_in_to_script;
-pub mod tx_in_to_script_hash;
-pub mod tx_in_to_key;
-pub mod tx_in;
+mod tx_in_gen;
+mod tx_in_to_script;
+mod tx_in_to_script_hash;
+mod tx_in_to_key;
+mod tx_in;
 
 pub struct RingSignature(pub Vec<Signature>);
 
