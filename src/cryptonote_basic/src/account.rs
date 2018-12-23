4b58f39215dceb8fbd3a78b43bdc980eaaa11d4d
LINE <769483043@qq.com>
2018-12-23 21:38:52 +0800

    fix account

--- src/cryptonote_basic/src/account.rs
+++ src/cryptonote_basic/src/account.rs
@@ -2,9 +2,9 @@ use crate::block::AccountPublicAddress;
 use xmr_crypto::crypto::SecretKey;
 
 pub struct AccountKeys {
-    accountAddress: AccountPublicAddress,
-    spendSecretKey: SecretKey,
-    vewSecretKey: SecretKey,
+    account_address: AccountPublicAddress,
+    spend_secret_key: SecretKey,
+    vew_secret_key: SecretKey,
     multisig_keys: Vec<SecretKey>,
     //TODO hw::device
     //hw::device *m_device = &hw::get_device("default");
