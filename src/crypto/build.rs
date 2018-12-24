6691e0bb907eb16c55e56fb6e2f2c57334f0d2b2
LINE <769483043@qq.com>
2018-12-24 14:40:13 +0800

    fix

--- src/crypto/build.rs
+++ src/crypto/build.rs
@@ -1,38 +1,34 @@
 extern crate cc;
 
 
-const CCRYPTO_PATH: &str = "ccrypto";
-const CCRYPTO_SOURCE_PATH: &str = CCRYPTO_PATH + "/src";
-
-
 fn main() {
     let mut build = cc::Build::new();
     let tool = build.get_compiler();
     if tool.is_like_clang() || tool.is_like_gnu() {
         build.flag_if_supported("-std=c99")
             .flag_if_supported("-msse4.1")
-            .flag_if_supported("-maes")
+            .flag_if_supported("-maes");
     }
 
     build.warnings(false);
-    build.file(CCRYPTO_SOURCE_PATH + "aesb.c")
-        .file(CCRYPTO_SOURCE_PATH + "blake256.c")
-        .file(CCRYPTO_SOURCE_PATH + "crypto-ops-data.c")
-        .file(CCRYPTO_SOURCE_PATH + "crypto-ops.c")
-        .file(CCRYPTO_SOURCE_PATH + "groestl.c")
-        .file(CCRYPTO_SOURCE_PATH + "hash-extra-blake.c")
-        .file(CCRYPTO_SOURCE_PATH + "hash-extra-groestl.c")
-        .file(CCRYPTO_SOURCE_PATH + "hash-extra-jh.c")
-        .file(CCRYPTO_SOURCE_PATH + "hash-extra-skein.c")
-        .file(CCRYPTO_SOURCE_PATH + "hash.c")
-        .file(CCRYPTO_SOURCE_PATH + "jh.c")
-        .file(CCRYPTO_SOURCE_PATH + "keccak.c")
-        .file(CCRYPTO_SOURCE_PATH + "oaes_lib.c")
-        .file(CCRYPTO_SOURCE_PATH + "random.c")
-        .file(CCRYPTO_SOURCE_PATH + "skein.c")
-        .file(CCRYPTO_SOURCE_PATH + "slow-hash.c")
-        .file(CCRYPTO_SOURCE_PATH + "tree-hash.c")
-        .include(CCRYPTO_SOURCE_PATH);
+    build.file("ccrypto/src/aesb.c")
+        .file("ccrypto/src/blake256.c")
+        .file("ccrypto/src/crypto-ops-data.c")
+        .file("ccrypto/src/crypto-ops.c")
+        .file("ccrypto/src/groestl.c")
+        .file("ccrypto/src/hash-extra-blake.c")
+        .file("ccrypto/src/hash-extra-groestl.c")
+        .file("ccrypto/src/hash-extra-jh.c")
+        .file("ccrypto/src/hash-extra-skein.c")
+        .file("ccrypto/src/hash.c")
+        .file("ccrypto/src/jh.c")
+        .file("ccrypto/src/keccak.c")
+        .file("ccrypto/src/oaes_lib.c")
+        .file("ccrypto/src/random.c")
+        .file("ccrypto/src/skein.c")
+        .file("ccrypto/src/slow-hash.c")
+        .file("ccrypto/src/tree-hash.c")
+        .include("ccrypto/src");
 
     build.compile("ccrypto");
 }
