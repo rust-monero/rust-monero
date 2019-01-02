2e617fae4176f14034cec49f904b2a9a226809d1
LINE <769483043@qq.com>
2019-01-02 18:22:19 +0800

    add constants

--- src/ringct/src/lib.rs
+++ src/ringct/src/lib.rs
@@ -1,9 +1,3 @@
 pub mod rct_types;
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
+pub mod rct_ops;
+pub mod multiexp;
---
+++ src/ringct/src/multiexp.rs
@@ -0,0 +1,12 @@
+use crate::rct_types::Key;
+use crypto::crypto_ops::ge_p3;
+
+pub struct MultiexpData {
+    scalar: Key,
+    point: ge_p3
+}
+
+
+//ringct/multiexp.h 56
+struct StrausCachedData;
+struct PippengerCachedData;
