af09c8fafb398e949f42c0918a836870fbb2d42f
LINE <769483043@qq.com>
2019-01-04 16:37:11 +0800

    add default method

--- src/crypto/src/hash.rs
+++ src/crypto/src/hash.rs
@@ -2,3 +2,9 @@ pub const HASH_SIZE: usize = 32;
 
 pub struct Hash(pub [u8; HASH_SIZE]);
 
+impl Default for Hash {
+    fn default() -> Self {
+        Hash([0; 32])
+    }
+}
+
--- src/db/src/blockchain_db.rs
+++ src/db/src/blockchain_db.rs
@@ -1,4 +1,5 @@
 use crypto::crypto::PublicKey;
+use crypto::hash::Hash;
 
 pub struct OutputData {
     pubkey: PublicKey,
@@ -7,6 +8,52 @@ pub struct OutputData {
 //    commitment: rct:key;
 }
 
-
-pub trait BlockChainDB {
+pub struct TxData {
+    pub tx_id: u64,
+    pub unlock_time: u64,
+    pub block_id: u64,
 }
+
+pub struct TxpoolTxMeta {
+    max_used_block_id: Hash,
+    last_failed_id: Hash,
+    weight: u64,
+    fee: u64,
+    max_used_block_height: u64,
+    last_failed_height: u64,
+    receive_time: u64,
+    last_relayed_time: u64,
+    // 112 bytes
+    kept_by_block: u8,
+    relayed: u8,
+    do_not_relay: u8,
+    //default 1
+    double_spend_seen: u8,
+    //default 7
+    bf_padding: u8,
+    padding: [u8; 76], // till 192 bytes
+}
+
+impl Default for TxpoolTxMeta {
+    fn default() -> Self {
+        TxpoolTxMeta {
+            max_used_block_id: Hash::default(),
+            last_failed_id: Hash::default(),
+            weight: 0,
+            fee: 0,
+            max_used_block_height: 0,
+            last_failed_height: 0,
+            receive_time: 0,
+            last_relayed_time: 0,
+            kept_by_block: 0,
+            relayed: 0,
+            do_not_relay: 0,
+            double_spend_seen: 1,
+            bf_padding: 7,
+            padding: [0; 76]
+        }
+    }
+}
+
+
+pub trait BlockChainDB {}
