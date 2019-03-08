c53738d4a1a6a44a89ccf03e198b1b4115d6a76f
LINE <769483043@qq.com>
2019-03-08 17:49:06 +0800

    dev

--- src/levin/src/section.rs
+++ src/levin/src/section.rs
@@ -1,10 +1,13 @@
 extern crate hex;
+extern crate rand;
 
 use std::collections::HashMap;
 
 use chrono::offset::TimeZone;
 use chrono::Utc;
-use hex::decode;
+use hex::*;
+
+use self::rand::Rng;
 
 pub struct Section {
     entries: HashMap<String, SectionValue>
@@ -33,14 +36,34 @@ impl Section {
     }
 
 
-    fn handshakeRequest() {
+    fn handshakeRequest() -> Section {
         let mut node_data = Section::new();
         node_data.add(String::from("local_time"), SectionValue::U64(Utc.timestamp().timestamp() as u64));
         node_data.add(String::from("myport"), SectionValue::U32(0));
 
         //TODO hex convert something
         //mainnet
-        let network_id = Hex::decode("1230f171610441611731008216a1a110");
+        let network_id = Hex::decode("1230f171610441611731008216a1a110").unwrap();
         node_data.add(String::from("network_id"), SectionValue::BYTES(network_id));
+
+        let mut rng = rand::thread_rng();
+        let peer_id = rng.gen::<u64>();
+        node_data.add(String::from("peer_id"), SectionValue::U64(peer_id));
+
+
+        //payload_data
+        let mut payload_data = Section::new();
+        payload_data.add(String::from("cumulative_difficulty"), SectionValue::U64(1));
+        payload_data.add(String::from("current_height"), SectionValue::U64(1));
+
+
+        let genesis_hash = Hex::decode("418015bb9ae982a1975da7d79277c2705727a56894ba0fb246adaabb1f4632e3").unwrap();
+        payload_data.add(String::from("top_id"), SectionValue::BYTES(genesis_hash));
+        payload_data.add(String::from("top_version"), SectionValue::BYTES([1 as byte]));
+
+        let mut section = Section::new();
+        section.add(String::from("node_data"), SectionValue::SECTION(node_data));
+        section.add(String::from("payload_data"), SectionValue::SECTION(payload_data));
+        return section;
     }
 }
