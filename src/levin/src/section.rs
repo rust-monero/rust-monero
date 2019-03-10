336d96ed6f58f34b39e5d9281b61064f2db19728
LINE <769483043@qq.com>
2019-03-10 09:52:04 +0800

    fix section

--- src/levin/src/section.rs
+++ src/levin/src/section.rs
@@ -3,21 +3,19 @@ extern crate rand;
 
 use std::collections::HashMap;
 
-use chrono::offset::TimeZone;
 use chrono::Utc;
-use hex::*;
 
 use self::rand::Rng;
 
 pub struct Section {
-    entries: HashMap<String, SectionValue>
+    pub entries: HashMap<String, SectionValue>
 }
 
 pub enum SectionValue {
     U32(u32),
     U64(u64),
     STRING(String),
-    BYTES([u8]),
+    BYTES(Vec<u8>),
     LIST(Vec<SectionValue>),
     SECTION(Section),
 }
@@ -30,20 +28,19 @@ impl Section {
         }
     }
 
-    fn add(mut self, key: String, value: SectionValue) -> Section {
+    fn add(&mut self, key: String, value: SectionValue) {
         self.entries.insert(key, value);
-        self
     }
 
 
     fn handshakeRequest() -> Section {
         let mut node_data = Section::new();
-        node_data.add(String::from("local_time"), SectionValue::U64(Utc.timestamp().timestamp() as u64));
+        node_data.add(String::from("local_time"), SectionValue::U64(Utc::now().timestamp_millis() as u64));
         node_data.add(String::from("myport"), SectionValue::U32(0));
 
         //TODO hex convert something
         //mainnet
-        let network_id = Hex::decode("1230f171610441611731008216a1a110").unwrap();
+        let network_id = hex::decode("1230f171610441611731008216a1a110").unwrap();
         node_data.add(String::from("network_id"), SectionValue::BYTES(network_id));
 
         let mut rng = rand::thread_rng();
@@ -57,13 +54,25 @@ impl Section {
         payload_data.add(String::from("current_height"), SectionValue::U64(1));
 
 
-        let genesis_hash = Hex::decode("418015bb9ae982a1975da7d79277c2705727a56894ba0fb246adaabb1f4632e3").unwrap();
+        let genesis_hash = hex::decode("418015bb9ae982a1975da7d79277c2705727a56894ba0fb246adaabb1f4632e3").unwrap();
         payload_data.add(String::from("top_id"), SectionValue::BYTES(genesis_hash));
-        payload_data.add(String::from("top_version"), SectionValue::BYTES([1 as byte]));
+        payload_data.add(String::from("top_version"), SectionValue::BYTES(vec!(1 as u8)));
 
         let mut section = Section::new();
         section.add(String::from("node_data"), SectionValue::SECTION(node_data));
         section.add(String::from("payload_data"), SectionValue::SECTION(payload_data));
         return section;
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use crate::section::Section;
+
+    #[test]
+    fn it_works() {
+        let s = Section::handshakeRequest();
+        assert_eq!(false, s.entries.is_empty());
     }
 }
