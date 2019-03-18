713e7f6d1469335c36b48b9e3e8896ec457c49cf
LINE <769483043@qq.com>
2019-03-18 23:00:50 +0800

    fix

--- src/levin/src/bucket.rs
+++ src/levin/src/bucket.rs
@@ -1,6 +1,8 @@
-use bytes::{Buf, BufMut, Bytes, BytesMut};
+use bytes::BytesMut;
 
-use crate::bucket_head::{BucketHead, LEVIN_PACKET_REQUEST, LEVIN_PROTOCOL_VER_1, LEVIN_SIGNATURE};
+use crate::bucket_head::{
+    BucketHead, LEVIN_PACKET_REQUEST, LEVIN_PACKET_RESPONSE, LEVIN_PROTOCOL_VER_1, LEVIN_SIGNATURE,
+};
 use crate::LevinError;
 use crate::section::Section;
 
@@ -9,23 +11,35 @@ pub struct Bucket {
     pub body: BytesMut,
 }
 
-
 impl Bucket {
-    fn create(command: u32, section: &mut Section) -> Result<Bucket, LevinError> {
+    fn create(
+        command: u32,
+        flags: u32,
+        have_to_return_data: bool,
+        section: &mut Section,
+    ) -> Result<Bucket, LevinError> {
         let mut body = BytesMut::new();
         section.write(&mut body);
         let bucket_head = BucketHead {
             signature: LEVIN_SIGNATURE,
             cb: body.len() as u64,
-            have_to_return_data: true,
+            have_to_return_data,
             command,
             return_code: 0,
-            flags: LEVIN_PACKET_REQUEST,
+            flags,
             protocol_version: LEVIN_PROTOCOL_VER_1,
         };
         Ok(Bucket {
             head: bucket_head,
-            body: body
+            body: body,
         })
+    }
+
+    fn create_request(command: u32, section: &mut Section) -> Result<Bucket, LevinError> {
+        Bucket::create(command, LEVIN_PACKET_REQUEST, true, section)
+    }
+
+    fn create_response(command: u32, section: &mut Section) -> Result<Bucket, LevinError> {
+        Bucket::create(command, LEVIN_PACKET_RESPONSE, false, section)
     }
 }
